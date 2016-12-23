ae3e08214210ae0010fbf1763925f2a2121d5de0
John Fourkiotis <john.fourkiotis@outlook.com>
2016-12-23 11:39:34 +0200

    missing files + folder restructuring

---
+++ go/src/monkey/evaluator/builtins.go
@@ -0,0 +1,34 @@
+package evaluator
+
+import (
+	"fmt"
+	"monkey/object"
+)
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
+		},
+	},
+}
